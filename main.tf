54b22c192d84eeb079adfb23fbcf026b7fb6215b
Tom Bell <tjbell@gmail.com>
2020-08-11 11:50:19 -0600

    chore: add custom script

--- main.tf
+++ main.tf
@@ -101,12 +101,24 @@ resource "azurerm_network_security_group" "lab_security_group" {
 
   security_rule {
     name                       = "SSH"
-    priority                   = 1001
+    priority                   = 101
     direction                  = "Inbound"
     access                     = "Allow"
     protocol                   = "Tcp"
     source_port_range          = "*"
     destination_port_range     = "22"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+  security_rule {
+    name                       = "HTTP"
+    priority                   = 102
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "80"
     source_address_prefix      = "*"
     destination_address_prefix = "*"
   }
